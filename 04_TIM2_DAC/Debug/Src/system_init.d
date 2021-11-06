513aff17ae664e4a38cb4f56d3697893c4856bf3
OlegDyakov <93733494+OlegDyakov@users.noreply.github.com>
2021-11-06 13:34:10 +0300

    Delete 04_TIM2_DAC/Debug/Src directory

--- 04_TIM2_DAC/Debug/Src/system_init.d
+++
@@ -1,4 +0,0 @@
-Src/system_init.o: ../Src/system_init.c \
- D:/Development/ST/Nucleo-32/STM32G431/Article/04_TIM2_DAC/Src/stm32g431.h
-
-D:/Development/ST/Nucleo-32/STM32G431/Article/04_TIM2_DAC/Src/stm32g431.h:
