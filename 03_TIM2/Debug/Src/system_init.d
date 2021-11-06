685a7ff1b78f8b381aa119dc4c5cdb02d39457a7
OlegDyakov <93733494+OlegDyakov@users.noreply.github.com>
2021-11-06 13:37:51 +0300

    Delete 03_TIM2 directory

--- 03_TIM2/Debug/Src/system_init.d
+++
@@ -1,4 +0,0 @@
-Src/system_init.o: ../Src/system_init.c \
- D:/Development/ST/Nucleo-32/STM32G431/Article/03_TIM2/Src/stm32g431.h
-
-D:/Development/ST/Nucleo-32/STM32G431/Article/03_TIM2/Src/stm32g431.h:
