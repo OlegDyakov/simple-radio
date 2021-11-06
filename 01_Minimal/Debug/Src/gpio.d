61ce550c9aad7fcad16c4ec21968d4d6c10b708e
OlegDyakov <93733494+OlegDyakov@users.noreply.github.com>
2021-11-06 14:17:01 +0300

    Delete 01_Minimal directory

--- 01_Minimal/Debug/Src/gpio.d
+++
@@ -1,4 +0,0 @@
-Src/gpio.o: ../Src/gpio.c \
- D:/Development/ST/Nucleo-32/STM32G431/Article/01_Minimal/Src/stm32g431.h
-
-D:/Development/ST/Nucleo-32/STM32G431/Article/01_Minimal/Src/stm32g431.h:
--- 01_Minimal/Debug/Src/main.d
+++
@@ -1,4 +0,0 @@
-Src/main.o: ../Src/main.c \
- D:/Development/ST/Nucleo-32/STM32G431/Article/01_Minimal/Src/stm32g431.h
-
-D:/Development/ST/Nucleo-32/STM32G431/Article/01_Minimal/Src/stm32g431.h:
