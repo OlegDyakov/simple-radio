94052b98accb717917331c54d3b1ed93c2b4621b
OlegDyakov <93733494+OlegDyakov@users.noreply.github.com>
2021-11-06 14:17:50 +0300

    Delete 05_TIM2_DAC_SIN directory

--- 05_TIM2_DAC_SIN/Debug/Src/dac.d
+++
@@ -1,4 +0,0 @@
-Src/dac.o: ../Src/dac.c \
- D:/Development/ST/Nucleo-32/STM32G431/Article/05_TIM2_DAC_SIN/Src/stm32g431.h
-
-D:/Development/ST/Nucleo-32/STM32G431/Article/05_TIM2_DAC_SIN/Src/stm32g431.h:
